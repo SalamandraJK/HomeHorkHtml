4fa035e35486b7b47a1f2e0792c57483a9ffd0f6
MikhailPL <muke.jry@gmail.com>
2023-06-15 15:22:12 +0300

    Added HW3

---
+++ HomeW2/script/scripts.js
@@ -0,0 +1,16 @@
+function funkName()
+{
+let name = prompt('Как вас зовут?', 'Имя');
+return name
+}
+alert(`Ну'с, здравствуйте, ${funkName()}`);
+
+let element = document.getElementById('Num5');
+console.log(element);
+
+let first_elem = document.getElementById('Null');
+console.log(first_elem);
+
+let push = document.getElementById('btn')
+console.log(push);
+push.addEventListener('click', chanchedcolor);
